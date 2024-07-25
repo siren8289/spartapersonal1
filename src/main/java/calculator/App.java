e0d25c183d5bd50a8e768ec635b85a142eaec4a1
siren8289 <siren8289@gmail.com>
2024-07-25 16:19:07 +0900

    Level1-3
    사칙연산 기호 사용해서 함

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -12,13 +12,31 @@ public class App {
         int num2 = sc.nextInt();
 
         System.out.println("첫 번째 숫자:" + num1);
-        System.out.println("두 번째 숫자:"+ num2);
+        System.out.println("두 번째 숫자:" + num2);
 
         System.out.print("사칙연산 기호를 입력하세요: ");
         String input = sc.next();
-        char Number = sc.next().charAt(0); //첫번째 글자만 받아오기 위해 CharAT(0)메서드 사용
+        char operator = sc.next().charAt(0); //첫번째 글자만 받아오기 위해 CharAT(0)메서드 사용
 
-
+        int result = 0;
+        if (operator == '+') {
+            result = num1 + num2;
+        } else if (operator == '-') {
+            result = num1 - num2;
+        } else if (operator == '*') {
+            result = num1 * num2;
+        } else if (operator == '/') {
+            if (num2 == 0) {
+                System.out.println("나눗셈 연산 안에 분모의 수가 0이 될 수 없습니다");
+                return; //프로그램 종료
+            } else {
+                result = num1 / num2;
+            }
+        } else {
+            System.out.println("올바른 사칙연산 기호를 입력하세요");
+            return; //프로그램 종료
+        }
+        System.out.println("결과: " + result);
     }
 }
 
