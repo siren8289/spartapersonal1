8a07a0b7a33679fd4c78332f3278a1c49cd43d04
siren8289 <siren8289@gmail.com>
2024-07-25 18:05:00 +0900

    Level1-4
    if->switch문으로 변경(변수 반복 오류 때문에)
    while(true)반복문 이용

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -4,39 +4,53 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        //  스캐너 객체 생성
         Scanner sc = new Scanner(System.in);
+        /* 반복문 사용 해서 연산을 반복 */
+        while (true) {
+            System.out.println("첫 번째 숫자 입력 (종료=exit):");
+            String input = sc.next();
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int num1 = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
-
-        System.out.println("첫 번째 숫자:" + num1);
-        System.out.println("두 번째 숫자:" + num2);
-
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        String input = sc.next();
-        char operator = sc.next().charAt(0); //첫번째 글자만 받아오기 위해 CharAT(0)메서드 사용
-
-        int result = 0;
-        if (operator == '+') {
-            result = num1 + num2;
-        } else if (operator == '-') {
-            result = num1 - num2;
-        } else if (operator == '*') {
-            result = num1 * num2;
-        } else if (operator == '/') {
-            if (num2 == 0) {
-                System.out.println("나눗셈 연산 안에 분모의 수가 0이 될 수 없습니다");
-                return; //프로그램 종료
-            } else {
-                result = num1 / num2;
+            if (input.equalsIgnoreCase("exit")) {
+                break;
             }
-        } else {
-            System.out.println("올바른 사칙연산 기호를 입력하세요");
-            return; //프로그램 종료
+
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
+
+            System.out.println("첫 번째 숫자: " + num1);
+            System.out.println("두 번째 숫자: " + num2);
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = sc.next().charAt(0);
+
+            int result = 0;
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("나눗셈 연산 안에 분모의 수가 0이 될 수 없습니다.");
+                        return; // 프로그램 종료
+                    } else {
+                        result = num1 / num2;
+                    }
+                    break;
+                default:
+                    System.out.println("올바른 사칙연산 기호를 입력하세요.");
+                    return; // 프로그램 종료
+            }
+            System.out.println("결과: " + result);
         }
-        System.out.println("결과: " + result);
     }
 }
 
